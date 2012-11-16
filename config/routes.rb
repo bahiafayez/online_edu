5d4bfd9cc14c7a1056d8eced091888ad8a702e58
Bahia ElSharkawy <bahia.sharkawy@gmail.com>
2012-11-16 16:29:00 +0200

    changing coordinates

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ OnlineEdu::Application.routes.draw do
   ActiveAdmin.routes(self)
 
   devise_for :admin_users, ActiveAdmin::Devise.config
-
+  get "home/index"
   #get "courses/new"
 
   #get "courses/index"
