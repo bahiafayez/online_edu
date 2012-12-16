0e2ace5b23f5fc34035ba16da9d696037dda7504
Bahia ElSharkawy <bahia.sharkawy@gmail.com>
2012-12-16 18:43:08 +0200

    changing confused time zone

--- app/models/confused.rb
+++ app/models/confused.rb
@@ -4,6 +4,7 @@ class Confused < ActiveRecord::Base
   
   def self.get_rounded_time(array)
     return_hash={}
+    Time.zone="UTC"
     array.each do |c|
         parsed_time=Time.zone.parse(Time.seconds_to_time(c.time+1)).ceil(1.minute).to_i #currently rounding to nearest minute. could change that. #to_i to use it in javascript
         return_hash[parsed_time] = (return_hash[parsed_time]||0) + 1
