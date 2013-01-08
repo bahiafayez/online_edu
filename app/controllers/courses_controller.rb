c692d030a01929959e7667299c14b7080a5b3441
Bahia ElSharkawy <bahia.sharkawy@gmail.com>
2013-01-08 14:29:03 +0200

    fixing name not found bug

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -332,7 +332,7 @@ class CoursesController < ApplicationController
     @students=@course.users
     @quizzes=@course.quizzes
     @student_names=[]
-   
+   puts @course.lectures
    
    if params[:all_modules]  #when click on the modules - all modules, progress of each student in each module (done/not done/done but not on time)
      @matrix={}
